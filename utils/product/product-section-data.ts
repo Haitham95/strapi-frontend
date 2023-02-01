3c4d65c487ebb3340fe369c713e30c9f268630e2
Haitham <haithamsm95@gmail.com>
2023-02-01 02:20:28 +0200

    updated landing page

---
+++ utils/product/product-section-data.ts
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 1,
+    name: "Black Basic Tee",
+    price: "$32",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-03-favorite-01.jpg",
+    imageAlt: "Model wearing women's black cotton crewneck tee.",
+  },
+  {
+    id: 2,
+    name: "Black Basic Tee",
+    price: "$32",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-03-favorite-01.jpg",
+    imageAlt: "Model wearing women's black cotton crewneck tee.",
+  },
+  {
+    id: 3,
+    name: "Black Basic Tee",
+    price: "$32",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-03-favorite-01.jpg",
+    imageAlt: "Model wearing women's black cotton crewneck tee.",
+  },
+  // More products...
+];
