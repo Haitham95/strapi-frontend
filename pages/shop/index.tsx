0d8ad587b6ce45dcfaeb1f281e0b8cdfbf98fdc7
Haitham <haithamsm95@gmail.com>
2023-02-02 04:47:18 +0200

    UI fixes

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,16 +1,61 @@
 import BreadcrumbDemo1 from "../../components/Breadcrumbs/BreadcrumbDemo1";
+import CategoriesFilterDesktop from "../../components/Category/CategoriesFilterDesktop";
+import CategoriesFilterMobile from "../../components/Category/CategoriesFilterMobile";
 import HeaderDemo2 from "../../components/Header/HeaderDemo2";
 import Layout from "../../components/Layout";
 import ShopLayout from "../../components/Layouts/ShopLayout";
+import ProductListDemo1 from "../../components/Product/ProductListDemo1";
+
+const quickLinks = [
+  { name: "Totes", href: "#" },
+  { name: "Backpacks", href: "#" },
+  { name: "Travel Bags", href: "#" },
+  { name: "Hip Bags", href: "#" },
+  { name: "Laptop Sleeves", href: "#" },
+];
+
+const filters = [
+  {
+    id: "color",
+    name: "Color",
+    options: [
+      { value: "white", label: "White", checked: false },
+      { value: "beige", label: "Beige", checked: false },
+      { value: "blue", label: "Blue", checked: true },
+      { value: "brown", label: "Brown", checked: false },
+      { value: "green", label: "Green", checked: false },
+      { value: "purple", label: "Purple", checked: false },
+    ],
+  },
+  {
+    id: "category",
+    name: "Category",
+    options: [
+      { value: "new-arrivals", label: "New Arrivals", checked: false },
+      { value: "sale", label: "Sale", checked: false },
+      { value: "travel", label: "Travel", checked: true },
+      { value: "organization", label: "Organization", checked: false },
+      { value: "accessories", label: "Accessories", checked: false },
+    ],
+  },
+  {
+    id: "size",
+    name: "Size",
+    options: [
+      { value: "2l", label: "2L", checked: false },
+      { value: "6l", label: "6L", checked: false },
+      { value: "12l", label: "12L", checked: false },
+      { value: "18l", label: "18L", checked: false },
+      { value: "20l", label: "20L", checked: false },
+      { value: "40l", label: "40L", checked: true },
+    ],
+  },
+];
 
 const Shop = () => (
   <>
     <Layout>
       {/* 
-        // 1. Indicator of where we are we in the shop
-        // 2. Header 
-        // 3. Sidebar for filtering
-        // 4. Products area
         // 5. Pagination
       */}
 
@@ -20,7 +65,22 @@ const Shop = () => (
         description="Thoughtfully designed objects for the workspace, home, and travel."
       />
       <main>
-        <ShopLayout />
+        <CategoriesFilterMobile filters={filters} quickLinks={quickLinks} />
+        <section aria-labelledby="products-heading" className="pt-6 pb-24">
+          <h2 id="products-heading" className="sr-only">
+            Products
+          </h2>
+          <ShopLayout>
+            {/* Filters */}
+            <CategoriesFilterDesktop
+              filters={filters}
+              quickLinks={quickLinks}
+            />
+            {/* Product grid */}
+
+            <ProductListDemo1 />
+          </ShopLayout>
+        </section>
       </main>
     </Layout>
   </>
